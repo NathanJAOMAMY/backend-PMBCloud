26ea7dcd69de1ad30d75224b7c39203e9f5a20f8
NathanJAOMAMY <nathanjaomamy@gmail.com>
2026-01-31 13:21:39 +0300

    fix supabase

--- src/middlewares/supabase.js
+++ src/middlewares/supabase.js
@@ -5,18 +5,7 @@ const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
 );
-const loginAndCheck = async ()=> {
-  const { error: signInError } = await supabase.auth.signInWithPassword({
-    email: "nadrasanamichael@gmail.com",
-    password: "0000",
-  });
 
-  if (signInError) {
-    console.error("Erreur de connexion:", signInError.message);
-    return;
-  }
-}
-loginAndCheck()
 
 module.exports = {
   supabase
